71770aad5e7a38029030ab928fb9987dfc04a7c8
Ubuntu <ubuntu@ip-172-31-3-248.ec2.internal>
2021-04-14 15:11:02 +0000

    Add scripts for search Conda recipes and Dockerfiles for commands

---
+++ grep-bioconda-recipes.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+# Print usage
+usage() {
+    cat << EOF
+
+NAME
+    grep-bioconda-recipes - search for commands in build scripts
+
+SYNOPSIS
+    grep-bioconda-recipes [-d bioconda-recipes-directory] [-c commands]
+
+DESCRIPTION
+    Uses grep to search for a space separated list of commands in
+    Bioconda recipes build scripts.
+
+    An output files is created for each command named
+    "grep-bioconda-recipes-${command}.log".
+
+OPTIONS 
+    -d    The bioconda-recipes directory
+    -c    A space separated list of commands, default: "ssh sftp scp
+          wget curl"
+
+EOF
+}
+
+# Parse command line options
+BIOCONDA_RECIPES_DIR="/home/ubuntu/bioconda-recipes"
+COMMANDS="ssh sftp scp wget curl"
+while getopts ":d:c:h" opt; do
+    case $opt in
+	d)
+	    BIOCONDA_RECIPES_DIR="${OPTARG}"
+	    ;;
+	c)
+	    COMMANDS="-${OPTARG}"
+	    ;;
+	h)
+	    usage
+	    exit 0
+	    ;;
+	\?)
+	    echo "Invalid option: -${OPTARG}" >&2
+	    usage
+	    exit 1
+	    ;;
+	\:)
+	    echo "Option -${OPTARG} requires an argument." >&2
+	    usage
+	    exit 1
+	    ;;
+    esac
+done
+
+# Parse command line arguments
+shift `expr ${OPTIND} - 1`
+if [ "$#" -ne 0 ]; then
+    echo "No arguments required"
+    exit 1
+fi
+
+# Setup
+set -e
+
+# Find and search recipes in background subshells
+for command in ${COMMANDS}; do
+    $(find ${BIOCONDA_RECIPES_DIR} -name "build.sh" \
+	   -exec grep -Hn ${command} {} \; \
+	  | tee grep-bioconda-recipes-${command}.log \
+		> /dev/null) &
+done
+
+# Teardown
+# Nothing required
---
+++ grep-containers.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+# Print usage
+usage() {
+    cat << EOF
+
+NAME
+    grep-containers - search for commands in Dockerfiles
+
+SYNOPSIS
+    grep-containers [-d containers-directory] [-c commands]
+
+DESCRIPTION
+    Uses grep to search for a space separated list of commands in
+    BioContainers Dockerfiles.
+
+    An output files is created for each command named
+    "grep-containers-${command}.log".
+
+OPTIONS 
+    -d    The containers directory
+    -c    A space separated list of commands, default: "ssh sftp scp
+          wget curl"
+
+EOF
+}
+
+# Parse command line options
+CONTAINERS_DIR="/home/ubuntu/containers"
+COMMANDS="ssh sftp scp wget curl"
+while getopts ":d:c:h" opt; do
+    case $opt in
+	d)
+	    CONTAINERS_DIR="${OPTARG}"
+	    ;;
+	c)
+	    COMMANDS="-${OPTARG}"
+	    ;;
+	h)
+	    usage
+	    exit 0
+	    ;;
+	\?)
+	    echo "Invalid option: -${OPTARG}" >&2
+	    usage
+	    exit 1
+	    ;;
+	\:)
+	    echo "Option -${OPTARG} requires an argument." >&2
+	    usage
+	    exit 1
+	    ;;
+    esac
+done
+
+# Parse command line arguments
+shift `expr ${OPTIND} - 1`
+if [ "$#" -ne 0 ]; then
+    echo "No arguments required"
+    exit 1
+fi
+
+# Setup
+set -e
+
+# Find and search recipes in background subshells
+for command in ${COMMANDS}; do
+    $(find ${CONTAINERS_DIR} -name "Dockerfile" \
+	   -exec grep -Hn ${command} {} \; \
+	  | tee grep-containers-${command}.log \
+		> /dev/null) &
+done
+
+# Teardown
+# Nothing required
