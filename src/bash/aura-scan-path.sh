2c84e3b89321a9777ea07b192a6130beaddd91c2
Raymond LeClair <raymond.leclair@springbok.io>
2021-05-26 14:54:14 +0000

    Allow a Python path or Git URL

--- src/bash/aura-scan-path.sh
+++ src/bash/aura-scan-path.sh
@@ -5,14 +5,15 @@ usage() {
     cat << EOF
 
 NAME
-    aura-scan - scan a Python file
+    aura-scan-path - Use Aura to scan a Python path or Git repository
 
 SYNOPSIS
-    aura-scan path-to-python-file path-to-scan-home
+    aura-scan-path [python-path|git-url] scan-home
 
 DESCRIPTION
-    Uses Aura to scan a python file and produce JSON output in the
-    scan home directory.
+    Uses Aura to scan a Python path or Git repository and produce JSON
+    output in the scan home directory. The Python path can be to an
+    individual Python file, or to a directory containing Python files.
 
 OPTIONS 
     None
@@ -46,21 +47,36 @@ if [ "$#" -ne 2 ]; then
     echo "Two arguments required"
     exit 1
 fi
-file_home=$(dirname "${1}")
-file_name=$(basename "${1}")
+if [ "${1: -4}" == ".git" ]; then
+    git_url="${1}"
+    python_name=$(basename "${1}" | sed s/.git//)
+    python_home=$(realpath ${python_name} | xargs dirname)
+    scan_name="${python_name}.json"
+else
+    git_url=""
+    python_name=$(realpath "${1}" | xargs basename)
+    python_home=$(realpath "${1}" | xargs dirname)
+    scan_name="$(echo "${python_name}" | sed s/.py//).json"
+fi
 scan_home="${2}"
 
 # Setup
 set -xe
+if [ -n "${git_url}" ]; then
+    rm -rf ${python_name}
+    git clone ${git_url}
+fi
+mkdir -p ${scan_home}
 
-# Scan a python file and produce JSON output
-scan_name=$(echo "${file_name}" \
-		| sed s/.py/.json/)
+# Use Aura to Scan a python file and produce JSON output
 docker run \
-       -v ${file_home}:/home \
+       -v ${python_home}:/home \
        --rm sourcecodeai/aura:dev scan \
-       /home/${file_name} -v -f json \
+       /home/${python_name} -v -f json \
        > ${scan_home}/${scan_name}
 
 # Teardown
 # Nothing required
+if [ -n "${git_url}" ]; then
+    rm -rf ${python_name}
+fi
