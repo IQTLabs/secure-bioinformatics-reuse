d72a05b15980359c2d11c7c919723c014d5f157f
Raymond LeClair <raymond.leclair@springbok.io>
2021-06-09 00:53:24 +0000

    Add module for anlayzing scan and trace results

---
+++ src/python/analyze.py
@@ -0,0 +1,47 @@
+import json
+import logging
+from pathlib import Path
+import pprint
+
+
+TARGET_DIR = Path("/home/ubuntu/target")
+SCAN_RESULTS_DIR = TARGET_DIR / "scan"
+SCAN_RESULTS_FILE = TARGET_DIR / SCAN_RESULTS_DIR.with_suffix(".json").name
+
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+
+ch = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+root.addHandler(ch)
+
+logger = logging.getLogger("analyze")
+
+
+def load_aura_scan_results(force=False):
+    """Deserialize Aura scan results and retain detections with a non-zero score. Collect the scan results and serialize to JSON for faster deserialization.
+    """
+    if SCAN_RESULTS_FILE.exists() and not force:
+        logger.info("Loading scan results file: {0}".format(SCAN_RESULTS_FILE))
+        with SCAN_RESULTS_FILE.open('r') as fp:
+            scan_results = json.load(fp)
+    else:
+        scan_results = []
+        for scan_path in SCAN_RESULTS_DIR.iterdir():
+            logger.info("Loading scan path: {0}".format(scan_path))
+            scan_result = json.loads(scan_path.read_text())
+            detections = []
+            for detection in scan_result['detections']:
+                if detection['score'] > 0:
+                    detections.append(detection)
+            scan_result['detections'] = detections
+            scan_results.append(scan_result)
+        logger.info("Dumping scan results file: {0}".format(SCAN_RESULTS_FILE))
+        with SCAN_RESULTS_FILE.open('w') as fp:
+            json.dump(scan_results, fp, indent=4)
+    return scan_results
+
+
+if __name__ == "__main__":
+    scan_results = load_aura_scan_results()
